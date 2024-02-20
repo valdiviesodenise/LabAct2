db7347391226a2a3e4487cda9ccf45c01f3c125b
DENISE ANNE VALDIVIESO <152182602+valdiviesodenise@users.noreply.github.com>
2024-02-20 12:27:52 +0000

    .

--- src/Card.js
+++ src/Card.js
@@ -1,29 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Card(props) {
-  const [showMessage, setShowMessage] = useState(false);
+  const [stockStatus, setStockStatus] = useState("In Stock");
+
+  useEffect(() => {
+    const isPalette = (productname) => {
+      return productname.toLowerCase().includes("palette");
+    };
+    if (isPalette(props.productname)) {
+      setStockStatus("Out of Stock");
+    } else {
+      setStockStatus("In Stock");
+    }
+  }, [props.productname]);
 
   const handleAddToCart = () => {
-    if (props.stock === "In Stock") {
-      // Perform add to cart logic here
-      setShowMessage(true);
-    }
+    setStockStatus("Successfully Added to Cart!");
+    setTimeout(() => {
+      setStockStatus("In Stock");
+    }, 700);
   };
+
+  let message = "Successfully Added to Cart!";
+
+  if (stockStatus === "Out of Stock!") {
+    message = "Out of Stock!";
+  }
 
   return (
     <div className="card">
       <img src={props.imgUrl} alt="card-img" className="card-img" />
       <h2>{props.productname}</h2>
       <p>{props.price}</p>
-      <button
-        className="button"
-        onClick={handleAddToCart}
-        disabled={props.stock === "Out of Stock"}
-      >
-        {props.stock === "Out of Stock" ? "Sold Out" : "Add to Cart"}
+      <button className="add-to-cart" onClick={handleAddToCart}>
+        Add to Cart
       </button>
-      {showMessage && (
-        <div className="popup-message">Successfully added to cart!</div>
+      {/* Pop-up */}
+      {stockStatus === "Successfully Added to Cart!" && (
+        <div className="popup">{message}</div>
       )}
     </div>
   );
